7cc48399ad9a7695ff3d75a21ecdfa251407a5fa
farisawan-2000 <farisawan.2000@gmail.com>
2021-04-27 23:50:18 -0400

    remove some unused sm64 remnant libultra files

--- src/os/func_802F71F0.c
+++
@@ -1,8 +0,0 @@
-#include "libultra_internal.h"
-
-void func_802F71F0(void) {
-    register u32 s0 = __osDisableInt();
-    __osRunningThread->state = OS_STATE_RUNNABLE;
-    __osEnqueueAndYield(&__osRunQueue);
-    __osRestoreInt(s0);
-}
--- src/os/kdebugserver_stack.c
+++
@@ -1,5 +0,0 @@
-#include "libultra_internal.h"
-
-#if defined(VERSION_JP) || defined(VERSION_US)
-u8 D_80365E40[0x100];
-#endif
