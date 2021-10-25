f45e95bc9c25aa1ab41a90fa9bb6ffe3a3a48c85
CoderStig <79828759+CoderStig@users.noreply.github.com>
2021-10-25 13:30:13 -0600

    Add files via upload

--- asm/bss_8028DF00.s
+++ asm/bss_8028DF00.s
@@ -33,6 +33,46 @@ glabel D_802BA27C
 .skip 4
 
 glabel D_802BA280
-.skip 16
+.skip 875776 # 16
 
-glabel D_802BA290
+
+
+
+#glabel D_802BA290
+#.skip 22768 # 100592
+
+#glabel D_802BFB80
+#.skip 77824
+
+#glabel D_802D2B80
+#.skip 32768
+
+#glabel D_802DAB80
+#.skip 20480
+
+#glabel D_802DFB80
+#.skip 74752
+
+#glabel D_802F1F80
+#.skip 32768
+
+#glabel gZBuffer
+#.skip 153600
+
+
+
+#glabel gFramebuffer0
+#.skip 153600
+
+#glabel gFramebuffer1
+#.skip 153600
+
+#glabel gFramebuffer2
+#.skip 153600
+
+## Audio
+
+glabel gGfxSPTaskOutputBuffer # 0x8038FF80
+.skip 129024
+
+glabel gGfxSPTaskOutputBufferSize # 0x803AF780
