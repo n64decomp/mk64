b56ab5cad77fdf3b3010813d890c26ca363ed36b
Taggerung <tyler.taggerung@gmail.com>
2022-01-03 20:08:34 -0500

    Add a couple header files
    These files contain funciton prototypes for matches functions, NON_MATCHING functions,
     and functions used by matched functions.
    
    At this point in time those headers may contain prototypes for functions defined in
     unrelated files, but fixing that will have take time
    
    Signed-off-by: Taggerung <tyler.taggerung@gmail.com>

---
+++ include/code_80005FD0.h
@@ -0,0 +1,15 @@
+#ifndef CODE_80005FD0_H
+#define CODE_80005FD0_H
+
+#include "common_structs.h"
+
+/* Function Prototypes */
+
+s16 func_80005FD0();
+
+// math_util.c
+u32 func_802B5224();
+
+/* This is where I'd put my static data, if I had any */
+
+#endif
---
+++ include/code_8008C1D0.h
@@ -0,0 +1,51 @@
+#ifndef CODE_8008C1D0_H
+#define CODE_8008C1D0_H
+
+#include "common_structs.h"
+
+/* Function Prototypes */
+
+void func_8008C310(Player*);
+void func_8008C354(Player*, s8);
+void func_8008C528(Player*, s8);
+void func_8008C62C(Player*, s8);
+void func_8008C6D0(Player*, s8);
+s32 func_8008D0E4();
+s32 func_8008D3B0();
+s32 func_8008D554();
+s32 func_8008D760();
+s32 func_8008D97C();
+s32 func_8008E884();
+s32 func_8008EC34();
+s32 func_8008EEC4();
+s32 func_8008F0E8(void *);
+s32 func_8008F3E0(void *);
+s32 func_8008F5A4(void *, s8);
+s32 func_8008FEDC(void *, s8);
+
+void func_8008C8C4(Player*, s8);
+void func_8008C6D0(Player*, s8);
+void func_800C90F4();
+void func_800C9060();
+void func_800098FC();
+
+// code_80057C60
+s32 func_8006B8B4();
+// code_80027D00.c
+s32 func_80031F48(Player*, float);
+
+/* This is where I'd put my static data, if I had any */
+
+extern u16 D_800DC51C;
+extern f32 D_800E37B0[];
+extern f32 D_800E3790[];
+
+extern s16 D_80165190[4][8];
+extern f32 D_80165280[];
+extern s32 D_80165420[];
+extern s32 D_80165440[];
+extern s32 D_80165460[];
+extern s32 D_80165480[];
+extern s32 D_8018D920[];
+
+#endif
