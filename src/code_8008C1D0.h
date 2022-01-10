32c0308deeab5dd7c7997a9389482892d283fe09
ChiefFruitcake <caminatorp@gmail.com>
2022-01-09 21:55:09 -0500

    Match functions in code_8008C1D0

--- src/code_8008C1D0.h
+++ src/code_8008C1D0.h
@@ -11,10 +11,10 @@ void func_8008C528(Player*, s8);
 void func_8008C62C(Player*, s8);
 void func_8008C6D0(Player*, s8);
 s32 func_8008D0E4();
-s32 func_8008D3B0();
-s32 func_8008D554();
-s32 func_8008D760();
-s32 func_8008D97C();
+void func_8008D3B0(Player*, s8);
+void func_8008D554(Player*);
+void func_8008D760(Player*);
+void func_8008D97C(Player*);
 s32 func_8008E884();
 s32 func_8008EC34();
 s32 func_8008EEC4();
@@ -37,15 +37,18 @@ s32 func_80031F48(Player*, float);
 /* This is where I'd put my static data, if I had any */
 
 extern u16 D_800DC51C;
+extern f32 D_800E3710[];
+extern f32 D_800E3730[];
 extern f32 D_800E37B0[];
 extern f32 D_800E3790[];
-
+extern f32 D_800EF568;
+extern f32 D_800EF56C;
 extern s16 D_80165190[4][8];
 extern f32 D_80165280[];
 extern s32 D_80165420[];
 extern s32 D_80165440[];
 extern s32 D_80165460[];
 extern s32 D_80165480[];
-extern s32 D_8018D920[];
+extern s16 D_8018D920[];
 
 #endif
