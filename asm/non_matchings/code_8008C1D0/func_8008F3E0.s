04097806a550c578aacdff42f6b55f18eb353e63
ChiefFruitcake <96551211+ChiefFruitcake@users.noreply.github.com>
2022-01-15 22:17:50 -0500

    code_8008C1D0 part 2 (#138)
    * Matching more functions in code_8008C1D0
    
    * More matches
    
    * Incorporate review comments. Match a few more functions

--- asm/non_matchings/code_8008C1D0/func_8008F3E0.s
+++
@@ -1,6 +0,0 @@
-glabel func_8008F3E0
-/* 08FFE0 8008F3E0 8C8E00BC */  lw    $t6, 0xbc($a0)
-/* 08FFE4 8008F3E4 2401BFFF */  li    $at, -16385
-/* 08FFE8 8008F3E8 01C17824 */  and   $t7, $t6, $at
-/* 08FFEC 8008F3EC 03E00008 */  jr    $ra
-/* 08FFF0 8008F3F0 AC8F00BC */   sw    $t7, 0xbc($a0)
