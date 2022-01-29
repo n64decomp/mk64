6b08e3be455e23ce6e596717a2f2720275ae8d8c
MegaMech <MegaMech@users.noreply.github.com>
2022-01-29 04:10:31 -0700

    Update osSyncPrintf.c

--- src/os/osSyncPrintf.c
+++ src/os/osSyncPrintf.c
@@ -1,23 +1,14 @@
 #include "libultra_internal.h"
 #include "libc/stdarg.h"
 
-// Fake matches
-char osSyncPrintf(int a, int b, int c) {
+s32 osSyncPrintf(s32 a, s32 b, s32 c) {
     // ifdef'd formatting code?
     return (1);
 }
 
-void func_800CF774(char *fmt, int a, char b, char c) {
-    char *args;
-    _Printf(osSyncPrintf, NULL, fmt, &a);
-}
-
-// Likely closer to actual.
-/*
 void func_800CF774(const char *fmt, ...) {
-  va_lista args;
+  va_list args;
   va_start(args, fmt);
   _Printf(osSyncPrintf, NULL, fmt, args);
   va_end(args);
 }
-*/
