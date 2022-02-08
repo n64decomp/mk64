1544235e1e205b336f6d5c25fa2ef96d59eb85e3
Taggerung <tyler.taggerung@gmail.com>
2022-02-07 23:39:40 -0500

    Add a header file for data_segment2
    Update a couple mips_2_c outputs based on the contents of the new header file
    
    Signed-off-by: Taggerung <tyler.taggerung@gmail.com>

---
+++ src/data_segment2.h
@@ -0,0 +1,80 @@
+#ifndef DATA_SEGMENT2_H
+#define DATA_SEGMENT2_H
+
+extern s32 D_02007B38[];
+
+extern Gfx D_02007650[];
+extern Gfx D_020076B0[];
+extern Gfx D_020076E0[];
+extern Gfx D_02007708[];
+extern Gfx D_02007728[];
+extern Gfx D_02007748[];
+extern Gfx D_02007768[];
+extern Gfx D_02007788[];
+extern Gfx D_020077A8[];
+extern Gfx D_020077D8[];
+extern Gfx D_020077F8[];
+extern Gfx D_02007818[];
+extern Gfx D_02007838[];
+extern Gfx D_02007858[];
+extern Gfx D_02007878[];
+extern Gfx D_02007898[];
+extern Gfx D_020078B8[];
+extern Gfx D_020078D8[];
+extern Gfx D_020078F8[];
+extern Gfx D_02007918[];
+extern Gfx D_02007938[];
+extern Gfx D_02007958[];
+extern Gfx D_02007978[];
+extern Gfx D_02007998[];
+extern Gfx D_020079B8[];
+extern Gfx D_020079D8[];
+extern Gfx D_020079F8[];
+extern Gfx D_02007A18[];
+extern Gfx D_02007A38[];
+extern Gfx D_02007A58[];
+extern Gfx D_02007A78[];
+extern Gfx D_02007A98[];
+extern Gfx D_02007AB8[];
+extern Gfx D_02007AD8[];
+extern Gfx D_02007AF8[];
+extern Gfx D_02007B18[];
+extern Gfx D_02007F18[];
+extern Gfx D_02007F48[];
+extern Gfx D_02007F60[];
+extern Gfx D_02007FC8[];
+extern Gfx D_02008008[];
+extern Gfx D_02008030[];
+extern Gfx D_02008058[];
+extern Gfx D_060093F8[];
+extern Gfx D_0D006940[];
+
+extern mk_Vtx_Flag D_02007BB8[];
+extern mk_Vtx_Flag D_02007BD8[];
+extern mk_Vtx_Flag D_02007BF8[];
+extern mk_Vtx_Flag D_02007C18[];
+extern mk_Vtx_Flag D_02007C38[];
+extern mk_Vtx_Flag D_02007C58[];
+extern mk_Vtx_Flag D_02007C78[];
+extern mk_Vtx_Flag D_02007C98[];
+extern mk_Vtx_Flag D_02007CB8[];
+extern mk_Vtx_Flag D_02007CD8[];
+extern mk_Vtx_Flag D_02007CF8[];
+extern mk_Vtx_Flag D_02007D18[];
+extern mk_Vtx_Flag D_02007D38[];
+extern mk_Vtx_Flag D_02007D58[];
+extern mk_Vtx_Flag D_02007D78[];
+extern mk_Vtx_Flag D_02007D98[];
+extern mk_Vtx_Flag D_02007DB8[];
+extern mk_Vtx_Flag D_02007DD8[];
+extern mk_Vtx_Flag D_02007DF8[];
+extern mk_Vtx_Flag D_02007E18[];
+extern mk_Vtx_Flag D_02007E38[];
+extern mk_Vtx_Flag D_02007E58[];
+extern mk_Vtx_Flag D_02007E78[];
+extern mk_Vtx_Flag D_02007E98[];
+extern mk_Vtx_Flag D_02007EB8[];
+extern mk_Vtx_Flag D_02007ED8[];
+extern mk_Vtx_Flag D_02007EF8[];
+
+#endif
