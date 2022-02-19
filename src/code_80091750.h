346f900d6029c0aa524e27b5fc7febf773e9c712
Taggerung <tyler.taggerung@gmail.com>
2022-02-18 21:46:18 -0500

    Match func_80091AC0
    Signed-off-by: Taggerung <tyler.taggerung@gmail.com>

--- src/code_80091750.h
+++ src/code_80091750.h
@@ -48,6 +48,7 @@ typedef struct {
 
 f64 exponent_by_squaring(f64, s32);
 f64 func_80091A6C(f64, s32);
+f64 func_80091AC0(f64, s32*);
 void swap_values(s32*, s32*);
 void func_80092148();
 void func_800921B4();
