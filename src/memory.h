45354a149b134ad745224818d8394bb2a4e7410b
Tyler McGavran <tyler.taggerung@gmail.com>
2022-04-18 10:04:46 -0400

    Mostly match a few functions in memory.c (#178)
    * Match a few functions in memory.c
    
    Signed-off-by: Taggerung <tyler.taggerung@gmail.com>

--- src/memory.h
+++ src/memory.h
@@ -11,6 +11,12 @@ s32 func_802A7B70(s32 segment);
 uintptr_t set_segment_base_addr(s32 segment, void *addr);
 void *get_segment_base_addr(s32 segment);
 s32 lookup_item(u32 segment);
+s8 func_802ABD10(u16);
+s16 func_802ABD40(u16);
+s16 func_802ABD7C(u16);
+s16 func_802ABDB8(u16);
+s16 func_802ABDF4(u16);
+f32 func_802ABE30(f32, f32, f32, u16);
 
 struct AllocOnlyPool {
     s32 totalSpace;
