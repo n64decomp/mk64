65ef8954cd39ae8dc364c9d626b8802d35c2115d
MegaMech <MegaMech@users.noreply.github.com>
2022-05-12 14:16:16 -0700

    Update extract_assets.py

--- extract_assets.py
+++ extract_assets.py
@@ -190,7 +190,7 @@ def main():
             print("extracting", asset)
             if "size" in meta:
                 # TODO: hack for extracting raw binary from MIO0 block
-                if assets[0][0].endswith(".bin"):
+                if assets[0][0].endswith(".bin") and magic != b"MIO0":
                     size = int(meta["size"], 0)
                 elif magic == b"MIO0":
 
