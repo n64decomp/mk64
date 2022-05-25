3b020c612d4e8a02336f8133174c2d0b2b6cae09
Taggerung <tyler.taggerung@gmail.com>
2022-05-25 19:23:56 -0400

    Match some audio functions
    Thank you Mech for figuring out what the stack management stuff was
     about.
    
    And yes, apparently the best way to match those functions is to have
     an `if` statement that uses an unset local variable as its condition
    
    Signed-off-by: Taggerung <tyler.taggerung@gmail.com>

---
+++ src/audio/external.h
@@ -0,0 +1,16 @@
+#ifndef AUDIO_EXTERNAL_H
+#define AUDIO_EXTERNAL_H
+
+#include "common_structs.h"
+
+void func_800CA0A0();
+void func_800CA0B8();
+void func_800CA0CC();
+void func_800CA270();
+void func_800CB134();
+
+extern s8  D_800EA0F4;
+extern s8  D_800EA108;
+extern s16 D_800EA174;
+
+#endif
