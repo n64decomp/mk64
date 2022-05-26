c82a7438fb695c376282ff130d5f854544bb4776
Tyler McGavran <tyler.taggerung@gmail.com>
2022-05-26 19:56:52 -0400

    Match some audio functions (#213)
    Thank you Mech for figuring out what the stack management stuff was
     about.
    
    And yes, apparently the best way to match those functions is to have
     an `if` statement that uses an unset local variable as its condition
    
    Signed-off-by: Taggerung <tyler.taggerung@gmail.com>

--- asm/non_matchings/audio/external/func_800CA0B8.s
+++
@@ -1,6 +0,0 @@
-glabel func_800CA0B8
-/* 0CACB8 800CA0B8 27BDFFF8 */  addiu $sp, $sp, -8
-/* 0CACBC 800CA0BC 3C01800F */  lui   $at, %hi(D_800EA108) # $at, 0x800f
-/* 0CACC0 800CA0C0 A020A108 */  sb    $zero, %lo(D_800EA108)($at)
-/* 0CACC4 800CA0C4 03E00008 */  jr    $ra
-/* 0CACC8 800CA0C8 27BD0008 */   addiu $sp, $sp, 8
