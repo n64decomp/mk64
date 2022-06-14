7072f36816df8285218fb689238cebe43ddd9244
MegaMech <MegaMech@users.noreply.github.com>
2022-06-14 01:00:19 -0700

    crash_screen decomp and mesc

--- asm/non_matchings/math_util/func_802B64B0.s
+++
@@ -1,6 +0,0 @@
-glabel func_802B64B0
-/* 11FAC0 802B64B0 AFA40000 */  sw    $a0, ($sp)
-/* 11FAC4 802B64B4 AFA50004 */  sw    $a1, 4($sp)
-/* 11FAC8 802B64B8 AFA60008 */  sw    $a2, 8($sp)
-/* 11FACC 802B64BC 03E00008 */  jr    $ra
-/* 11FAD0 802B64C0 AFA7000C */   sw    $a3, 0xc($sp)
