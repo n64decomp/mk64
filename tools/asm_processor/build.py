38584f7cb795783c4b6e13ba01032477fdf75a45
tehzz <tehzz@users.noreply.github.com>
2022-06-19 15:16:34 -0400

    update asm_processor for new ASM directives

--- tools/asm_processor/build.py
+++ tools/asm_processor/build.py
@@ -1,37 +1,114 @@
 #!/usr/bin/env python3
 import sys
-import os
+from pathlib import Path
 import shlex
 import subprocess
 import tempfile
+import uuid
+import asm_processor
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-asm_processor = ['python3', os.path.join(dir_path, "asm-processor.py")]
-prelude = os.path.join(dir_path, "prelude.inc")
+# Boolean for debugging purposes
+# Preprocessed files are temporary, set to True to keep a copy
+keep_preprocessed_files = False
+
+dir_path = Path(__file__).resolve().parent
+asm_prelude_path = dir_path / "prelude.inc"
 
 all_args = sys.argv[1:]
-sep1 = all_args.index('--')
-sep2 = all_args.index('--', sep1+1)
+sep0 = next(index for index, arg in enumerate(all_args) if not arg.startswith("-"))
+sep1 = all_args.index("--")
+sep2 = all_args.index("--", sep1 + 1)
 
-compiler = all_args[:sep1]
+asmproc_flags = all_args[:sep0]
+compiler = all_args[sep0:sep1]
 
-assembler = all_args[sep1+1:sep2]
-assembler_sh = ' '.join(shlex.quote(x) for x in assembler)
+assembler_args = all_args[sep1 + 1 : sep2]
+assembler_sh = " ".join(shlex.quote(x) for x in assembler_args)
 
-compile_args = all_args[sep2+1:]
-in_file = compile_args[-1]
-out_ind = compile_args.index('-o')
-out_file = compile_args[out_ind + 1]
+
+compile_args = all_args[sep2 + 1 :]
+
+in_file = Path(compile_args[-1])
 del compile_args[-1]
+
+out_ind = compile_args.index("-o")
+out_file = Path(compile_args[out_ind + 1])
 del compile_args[out_ind + 1]
 del compile_args[out_ind]
 
-in_dir = os.path.split(os.path.realpath(in_file))[0]
-opt_flags = [x for x in compile_args if x in ['-g', '-O2', '-framepointer']]
 
+in_dir = in_file.resolve().parent
+opt_flags = [
+    x for x in compile_args if x in {"-g3", "-g", "-O0", "-O1", "-O2", "-framepointer"}
+]
+if "-mips2" not in compile_args:
+    opt_flags.append("-mips1")
 
-preprocessed_file = tempfile.NamedTemporaryFile(prefix='preprocessed', suffix='.c', delete=False)
+asmproc_flags += opt_flags + [str(in_file)]
 
-subprocess.check_call(asm_processor + opt_flags + [in_file], stdout=preprocessed_file)
-subprocess.check_call(compiler + compile_args + ['-I', in_dir, '-o', out_file, preprocessed_file.name])
-subprocess.check_call(asm_processor + opt_flags + [in_file, '--post-process', out_file, '--assembler', assembler_sh, '--asm-prelude', prelude])
+# Drop .mdebug and .gptab sections from resulting binaries. This makes
+# resulting .o files much smaller and speeds up builds, but loses line
+# number debug data.
+# asmproc_flags += ["--drop-mdebug-gptab"]
+
+# Convert encoding before compiling.
+# asmproc_flags += ["--input-enc", "utf-8", "--output-enc", "euc-jp"]
+
+with tempfile.TemporaryDirectory(prefix="asm_processor") as tmpdirname:
+    tmpdir_path = Path(tmpdirname)
+    preprocessed_filename = "preprocessed_" + uuid.uuid4().hex + ".c"
+    preprocessed_path = tmpdir_path / preprocessed_filename
+
+    with preprocessed_path.open("wb") as f:
+        functions, deps = asm_processor.run(asmproc_flags, outfile=f)
+
+    if keep_preprocessed_files:
+        import shutil
+
+        keep_output_dir = Path("./asm_processor_preprocessed")
+        keep_output_dir.mkdir(parents=True, exist_ok=True)
+
+        shutil.copy(
+            preprocessed_path,
+            keep_output_dir / (in_file.stem + "_" + preprocessed_filename),
+        )
+
+    compile_cmdline = (
+        compiler
+        + compile_args
+        + ["-I", str(in_dir), "-o", str(out_file), str(preprocessed_path)]
+    )
+
+    try:
+        subprocess.check_call(compile_cmdline)
+    except subprocess.CalledProcessError as e:
+        print("Failed to compile file " + str(in_file) + ". Command line:")
+        print()
+        print(" ".join(shlex.quote(x) for x in compile_cmdline))
+        print()
+        sys.exit(55)
+
+    asm_processor.run(
+        asmproc_flags
+        + [
+            "--post-process",
+            str(out_file),
+            "--assembler",
+            assembler_sh,
+            "--asm-prelude",
+            str(asm_prelude_path),
+        ],
+        functions=functions,
+    )
+
+    deps_file = out_file.with_suffix(".asmproc.d")
+    if deps:
+        with deps_file.open("w") as f:
+            f.write(str(out_file) + ": " + " \\\n    ".join(deps) + "\n")
+            for dep in deps:
+                f.write("\n" + dep + ":\n")
+    else:
+        try:
+            deps_file.unlink()
+        except OSError:
+            pass
