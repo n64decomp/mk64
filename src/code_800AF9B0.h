2f710e9879e5bf79544caf5404e6ea59e02aa5bb
tehzz <tehzz@users.noreply.github.com>
2022-06-20 18:39:46 -0400

    decomp func_800AF9E4

--- src/code_800AF9B0.h
+++ src/code_800AF9B0.h
@@ -5,11 +5,11 @@
 #include <types.h>
 
 /* Function Prototypes */
-void func_800AF9B0();
+void func_800AF9B0(void);
 
 /* This is where I'd put my static data, if I had any */
 
-extern s32         gCCSelection; // D_800DC538
+extern s32         gCCSelection; // D_800DC538 in main.c
 extern s32         gModeSelection; // D_800DC53C
 extern s32         D_800DC540;
 extern s16         gCurrentCourseId; // D_800DC5A0
