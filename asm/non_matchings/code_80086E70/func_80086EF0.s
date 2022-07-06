778543d3ea93c14c971e577165b1c6e3f1e3ea11
MegaMech <MegaMech@users.noreply.github.com>
2022-07-06 00:07:18 -0700

    Decomp every func under 500 bytes (except audio)

--- asm/non_matchings/code_80086E70/func_80086EF0.s
+++
@@ -1,9 +0,0 @@
-glabel func_80086EF0
-/* 087AF0 80086EF0 27BDFFE8 */  addiu $sp, $sp, -0x18
-/* 087AF4 80086EF4 AFBF0014 */  sw    $ra, 0x14($sp)
-/* 087AF8 80086EF8 0C021B9C */  jal   func_80086E70
-/* 087AFC 80086EFC 00000000 */   nop   
-/* 087B00 80086F00 8FBF0014 */  lw    $ra, 0x14($sp)
-/* 087B04 80086F04 27BD0018 */  addiu $sp, $sp, 0x18
-/* 087B08 80086F08 03E00008 */  jr    $ra
-/* 087B0C 80086F0C 00000000 */   nop   
--- asm/non_matchings/code_80091750/func_8009969C.s
+++
@@ -1,9 +0,0 @@
-glabel func_8009969C
-/* 09A29C 8009969C 27BDFFE8 */  addiu $sp, $sp, -0x18
-/* 09A2A0 800996A0 AFBF0014 */  sw    $ra, 0x14($sp)
-/* 09A2A4 800996A4 0C0265AF */  jal   func_800996BC
-/* 09A2A8 800996A8 24050001 */   li    $a1, 1
-/* 09A2AC 800996AC 8FBF0014 */  lw    $ra, 0x14($sp)
-/* 09A2B0 800996B0 27BD0018 */  addiu $sp, $sp, 0x18
-/* 09A2B4 800996B4 03E00008 */  jr    $ra
-/* 09A2B8 800996B8 00000000 */   nop   
