5d9e30a5d0d83c29c3d90e8aed9695389c367857
MegaMech <MegaMech@users.noreply.github.com>
2022-07-09 04:02:47 -0700

    Decomp 800 byte sized funcs and below

--- asm/non_matchings/code_80086E70/func_8008B928.s
+++ asm/non_matchings/code_80086E70/func_8008B928.s
@@ -1,19 +1,4 @@
-glabel func_8008B8F0
-/* 08C4F0 8008B8F0 000470C0 */  sll   $t6, $a0, 3
-/* 08C4F4 8008B8F4 01C47023 */  subu  $t6, $t6, $a0
-/* 08C4F8 8008B8F8 3C0F8016 */  lui   $t7, %hi(D_80165C18) # $t7, 0x8016
-/* 08C4FC 8008B8FC AFA7000C */  sw    $a3, 0xc($sp)
-/* 08C500 8008B900 25EF5C18 */  addiu $t7, %lo(D_80165C18) # addiu $t7, $t7, 0x5c18
-/* 08C504 8008B904 000E7140 */  sll   $t6, $t6, 5
-/* 08C508 8008B908 44856000 */  mtc1  $a1, $f12
-/* 08C50C 8008B90C 44867000 */  mtc1  $a2, $f14
-/* 08C510 8008B910 C7A4000C */  lwc1  $f4, 0xc($sp)
-/* 08C514 8008B914 01CF1021 */  addu  $v0, $t6, $t7
-/* 08C518 8008B918 E44C0038 */  swc1  $f12, 0x38($v0)
-/* 08C51C 8008B91C E44E003C */  swc1  $f14, 0x3c($v0)
-/* 08C520 8008B920 03E00008 */  jr    $ra
-/* 08C524 8008B924 E4440040 */   swc1  $f4, 0x40($v0)
-
+glabel func_8008B928
 /* 08C528 8008B928 27BDFFE0 */  addiu $sp, $sp, -0x20
 /* 08C52C 8008B92C AFA50024 */  sw    $a1, 0x24($sp)
 /* 08C530 8008B930 AFA60028 */  sw    $a2, 0x28($sp)
@@ -85,6 +70,7 @@ glabel func_8008B8F0
 /* 08C638 8008BA38 03E00008 */  jr    $ra
 /* 08C63C 8008BA3C 27BD0020 */   addiu $sp, $sp, 0x20
 
+glabel func_8008BA40
 /* 08C640 8008BA40 27BDFFE8 */  addiu $sp, $sp, -0x18
 /* 08C644 8008BA44 AFBF0014 */  sw    $ra, 0x14($sp)
 /* 08C648 8008BA48 AFA40018 */  sw    $a0, 0x18($sp)
