3ab6d0fc304dd4d4783e682705d6fceae043360c
Taggerung <tyler.taggerung@gmail.com>
2023-03-22 19:29:10 -0400

    Relocated rodata for code_800431B0
    I would have liked to have figured out its plain
     data, but I was unable too. Maybe some other day.
    
    Signed-off-by: Taggerung <tyler.taggerung@gmail.com>

--- asm/non_matchings/code_800431B0/func_800534E8.s
+++ asm/non_matchings/code_800431B0/func_800534E8.s
@@ -1,3 +1,11 @@
+.section .late_rodata
+
+glabel jpt_800EE068
+.word L80053520, L80053574, L800535C8, L8005361C
+.word L80053670
+
+.section .text
+
 glabel func_800534E8
 /* 0540E8 800534E8 000470C0 */  sll   $t6, $a0, 3
 /* 0540EC 800534EC 01C47023 */  subu  $t6, $t6, $a0
