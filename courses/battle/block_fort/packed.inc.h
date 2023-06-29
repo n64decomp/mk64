bca10b2bcee486c789db9cf7c22911cdef756360
Taggerung <tyler.taggerung@email.com>
2023-06-29 01:00:36 -0400

    Link Block Fort packed DL's to course data
    Signed-off-by: Taggerung <tyler.taggerung@email.com>

---
+++ courses/battle/block_fort/packed.inc.h
@@ -0,0 +1,30 @@
+#include <PR/gbi.h>
+
+extern Gfx d_course_block_fort_packed_dl_0[];
+extern Gfx d_course_block_fort_packed_dl_B8[];
+extern Gfx d_course_block_fort_packed_dl_148[];
+extern Gfx d_course_block_fort_packed_dl_1D8[];
+extern Gfx d_course_block_fort_packed_dl_268[];
+extern Gfx d_course_block_fort_packed_dl_330[];
+extern Gfx d_course_block_fort_packed_dl_3F0[];
+extern Gfx d_course_block_fort_packed_dl_428[];
+extern Gfx d_course_block_fort_packed_dl_4C0[];
+extern Gfx d_course_block_fort_packed_dl_558[];
+extern Gfx d_course_block_fort_packed_dl_5F0[];
+extern Gfx d_course_block_fort_packed_dl_688[];
+extern Gfx d_course_block_fort_packed_dl_6B0[];
+extern Gfx d_course_block_fort_packed_dl_7A0[];
+extern Gfx d_course_block_fort_packed_dl_828[];
+extern Gfx d_course_block_fort_packed_dl_8B0[];
+extern Gfx d_course_block_fort_packed_dl_938[];
+extern Gfx d_course_block_fort_packed_dl_9C0[];
+extern Gfx d_course_block_fort_packed_dl_9F0[];
+extern Gfx d_course_block_fort_packed_dl_C80[];
+extern Gfx d_course_block_fort_packed_dl_F10[];
+extern Gfx d_course_block_fort_packed_dl_11A8[];
+extern Gfx d_course_block_fort_packed_dl_14A0[];
+extern Gfx d_course_block_fort_packed_dl_14C8[];
+extern Gfx d_course_block_fort_packed_dl_1580[];
+extern Gfx d_course_block_fort_packed_dl_1590[];
+extern Gfx d_course_block_fort_packed_dl_15C0[];
+extern Gfx d_course_block_fort_packed_dl_15D0[];
