f459f0cd0ffa2644a884149fe4ca02d325ad42fb
MegaMech <MegaMech@users.noreply.github.com>
2023-11-15 13:53:37 -0700

    Create startup_logo.inc.c

---
+++ yamls/startup_logo.inc.c
@@ -0,0 +1,145 @@
+dl1:
+  symbol: startup_logo_dl
+  type: gfx
+  mio0: 0x825800
+  offset: 0x2B00
+dl2:
+  symbol: startup_logo_dl2
+  type: gfx
+  mio0: 0x825800
+  offset: 0x2C88
+dl3:
+  symbol: startup_logo_dl3
+  type: gfx
+  mio0: 0x825800
+  offset: 0x2D58
+dl4:
+  symbol: startup_logo_dl4
+  type: gfx
+  mio0: 0x825800
+  offset: 0x2F20
+dl5:
+  symbol: startup_logo_dl5
+  type: gfx
+  mio0: 0x825800
+  offset: 0x2FF0
+dl6:
+  symbol: startup_logo_dl6
+  type: gfx
+  mio0: 0x825800
+  offset: 0x3180
+dl7:
+  symbol: startup_logo_dl7
+  type: gfx
+  mio0: 0x825800
+  offset: 0x3308
+dl8:
+  symbol: startup_logo_dl8
+  type: gfx
+  mio0: 0x825800
+  offset: 0x33D8
+startup_logo2:
+  symbol: startup_logo2
+  type: vtx
+  count: 1093
+  offset: 0x3538
+  mio0: 0x825800
+dl9:
+  symbol: startup_logo_dl9
+  type: gfx
+  mio0: 0x825800
+  offset: 0x7988
+dl10:
+  symbol: startup_logo_dl10
+  type: gfx
+  mio0: 0x825800
+  offset: 0x7C18
+dl11:
+  symbol: startup_logo_dl11
+  type: gfx
+  mio0: 0x825800
+  offset: 0x7CE8
+dl12:
+  symbol: startup_logo_dl12
+  type: gfx
+  mio0: 0x825800
+  offset: 0x7C50
+dl13:
+  symbol: startup_logo_dl13
+  type: gfx
+  mio0: 0x825800
+  offset: 0x7E50
+dl14:
+  symbol: startup_logo_dl14
+  type: gfx
+  mio0: 0x825800
+  offset: 0x7E90
+dl15:
+  symbol: startup_logo_dl15
+  type: gfx
+  mio0: 0x825800
+  offset: 0x7ED0
+dl16:
+  symbol: startup_logo_dl16
+  type: gfx
+  mio0: 0x825800
+  offset: 0x7F88
+dl17:
+  symbol: startup_logo_dl17
+  type: gfx
+  mio0: 0x825800
+  offset: 0x80F0
+dl18:
+  symbol: startup_logo_dl18
+  type: gfx
+  mio0: 0x825800
+  offset: 0x8250
+dl19:
+  symbol: startup_logo_dl19
+  type: gfx
+  mio0: 0x825800
+  offset: 0x83C8
+dl20:
+  symbol: startup_logo_dl20
+  type: gfx
+  mio0: 0x825800
+  offset: 0x8548
+dl21:
+  symbol: startup_logo_dl21
+  type: gfx
+  mio0: 0x825800
+  offset: 0x87A0
+reflection_map_gold:
+  symbol: reflection_map_gold
+  type: texture
+  mio0: 0x825800
+  offset: 0x8A48
+  size: 2048
+  width: 32
+  height: 32
+  format: RGBA16
+startup_texture_dl1:
+  symbol: startup_texture_dl1
+  type: gfx
+  mio0: 0x825800
+  offset: 0x9248
+lights:
+  symbol: lights
+  type: lights
+  mio0: 0x825800
+  offset: 0x9308
+startup_texture_dl2:
+  symbol: startup_texture_dl2
+  type: gfx
+  mio0: 0x825800
+  offset: 0x9320
+startup_texture_dl3:
+  symbol: startup_texture_dl3
+  type: gfx
+  mio0: 0x825800
+  offset: 0x93F8
+startup_texture_dl4:
+  symbol: startup_texture_dl4
+  type: gfx
+  mio0: 0x825800
+  offset: 0x9410
