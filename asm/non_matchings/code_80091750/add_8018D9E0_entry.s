fe6a54ba2f3254875901a8a09c93042e65206d62
Taggerung <tyler.taggerung@email.com>
2023-12-24 20:46:51 -0500

    Some matches in code_80091750
    Signed-off-by: Taggerung <tyler.taggerung@email.com>

--- asm/non_matchings/code_80091750/add_8018D9E0_entry.s
+++ asm/non_matchings/code_80091750/add_8018D9E0_entry.s
@@ -648,11 +648,11 @@ glabel L8009EE74
 /* 09FA9C 8009EE9C 00402025 */   move  $a0, $v0
 /* 09FAA0 8009EEA0 8FA60024 */  lw    $a2, 0x24($sp)
 /* 09FAA4 8009EEA4 8FBF007C */  lw    $ra, 0x7c($sp)
-/* 09FAA8 8009EEA8 3C04800E */  lui   $a0, %hi(D_800E7CA8)
+/* 09FAA8 8009EEA8 3C04800E */  lui   $a0, %hi(D_800E7D54 - 0xAC)
 /* 09FAAC 8009EEAC 00862021 */  addu  $a0, $a0, $a2
 /* 09FAB0 8009EEB0 AFE20018 */  sw    $v0, 0x18($ra)
 /* 09FAB4 8009EEB4 0C026449 */  jal   segmented_to_virtual_dupe
-/* 09FAB8 8009EEB8 8C847CA8 */   lw    $a0, %lo(D_800E7CA8)($a0)
+/* 09FAB8 8009EEB8 8C847CA8 */   lw    $a0, %lo(D_800E7D54 - 0xAC)($a0)
 /* 09FABC 8009EEBC 00402025 */  move  $a0, $v0
 /* 09FAC0 8009EEC0 0C0265AF */  jal   func_800996BC
 /* 09FAC4 8009EEC4 00002825 */   move  $a1, $zero
--- src/ending/code_80281C40.c
+++ src/ending/code_80281C40.c
@@ -22,7 +22,7 @@ struct UnkStruct80287560 {
 }; // 0xC
 
 /** bss **/
-u32 gMenuSelectionFromEndingSequence;
+s32 gMenuSelectionFromEndingSequence;
 s32 D_80287554;
 s32 D_80281C40_pad[2];
 struct UnkStruct80287560 D_80287560[30];
