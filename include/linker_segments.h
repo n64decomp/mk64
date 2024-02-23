2a8b656d1b4440682c251fbdc8bdb5535d4960ab
MegaMech <MegaMech@users.noreply.github.com>
2024-02-22 20:17:27 -0700

    test linker_segments removal

--- include/linker_segments.h
+++ include/linker_segments.h
@@ -7,13 +7,6 @@
  **/
 
 
-#define LINKER_MAIN        0x80000400
 
-#define LINKER_ENDING      0x80280000
-
-#define LINKER_RACING      0x8028DF00
-
-// End of Jumper pak memory
-#define RDRAM_END         0x807FFFFF
 
 #endif /* HARDCODED_SEGMENTS_H */
