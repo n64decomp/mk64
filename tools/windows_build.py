959eebc4141afaf09c260390430bebc1830542ca
coco875 <59367621+coco875@users.noreply.github.com>
2024-04-10 20:30:00 +0200

    remove windows build and combine linux test with page doxygen

--- tools/windows_build.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-bp = subprocess.Popen("tools/mingw64/w64devkit.exe", stdin=subprocess.PIPE)
-bp.stdin.write(b"make assets -j && make -j && exit\n")
-bp.stdin.close()
-exit(bp.wait())
