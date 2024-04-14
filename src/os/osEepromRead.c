f14c61ec9784d7573c2cc1bdd81b5415ab018e27
MegaMech <inkstudios1@hotmail.com>
2024-04-14 17:15:18 -0600

    Update osEeprom Read and Write

--- src/os/osEepromRead.c
+++ src/os/osEepromRead.c
@@ -1,15 +1,11 @@
 #include "libultra_internal.h"
+#include "controller.h"
+#include "macros.h"
 
-extern u8 _osLastSentSiCmd;
+extern u8 __osContLastCmd;
 
-extern u32 __osEepPifRam[15];
-extern u32 D_8019769C;
+extern OSPifRam __osEepPifRam;
 
-typedef struct {
-    u16 unk00;
-    u8 unk02;
-    u8 unk03;
-} unkStruct;
 typedef struct {
     u8 unk00;
     u8 unk01;
@@ -29,74 +25,84 @@ typedef struct {
     unkStruct3 unk04;
 } unkStruct2;
 
-s32 __osEepStatus(OSMesgQueue *, unkStruct *);
-s32 __osPackEepReadData(u8);
+s32 __osEepStatus(OSMesgQueue *, OSContStatus *);
+void __osPackEepReadData(u8);
 
 s32 osEepromRead(OSMesgQueue *mq, u8 address, u8 *buffer) {
-    s32 sp34;
-    s32 sp30;
-    u8 *sp2c;
-    unkStruct sp28;
-    unkStruct2 sp20;
-    sp34 = 0;
-    sp30 = 0;
-    sp2c = (u8 *) &__osEepPifRam;
-    if (address > 0x40) {
+    s32 ret = 0;
+    s32 i = 0;
+    u8 *ptr = (u8 *) &__osEepPifRam.ramarray;
+    OSContStatus sdata;
+    __OSContEepromFormat eepromformat;
+
+    if (address > EEPROM_MAXBLOCKS) {
         return -1;
     }
-    __osSiGetAccess();
-    sp34 = __osEepStatus(mq, &sp28);
-    if (sp34 != 0 || sp28.unk00 != 0x8000) {
 
-        return 8;
+    __osSiGetAccess();
+    ret = __osEepStatus(mq, &sdata);
+    if (ret != 0 || sdata.type != CONT_EEPROM) {
+
+        return CONT_NO_RESPONSE_ERROR;
     }
-    while (sp28.unk02 & 0x80) {
-        __osEepStatus(mq, &sp28);
+
+    while (sdata.status & CONT_EEPROM_BUSY) {
+        __osEepStatus(mq, &sdata);
     }
     __osPackEepReadData(address);
-    sp34 = __osSiRawStartDma(OS_WRITE, &__osEepPifRam);
+
+    ret = __osSiRawStartDma(OS_WRITE, &__osEepPifRam);
     osRecvMesg(mq, NULL, OS_MESG_BLOCK);
-    for (sp30 = 0; sp30 < 0x10; sp30++) {
-        __osEepPifRam[sp30] = 255;
+
+    for (i = 0; i < ARRAY_COUNT(__osEepPifRam.ramarray) + 1; i++) {
+        __osEepPifRam.ramarray[i] = CONT_CMD_NOP;
     }
-    D_8019769C = 0;
-    sp34 = __osSiRawStartDma(OS_READ, __osEepPifRam);
-    _osLastSentSiCmd = 4;
+    __osEepPifRam.pifstatus = 0;
+
+    ret = __osSiRawStartDma(OS_READ, &__osEepPifRam);
+    __osContLastCmd = CONT_CMD_READ_EEPROM;
     osRecvMesg(mq, NULL, OS_MESG_BLOCK);
-    for (sp30 = 0; sp30 < 4; sp30++) {
-        sp2c++;
+
+    for (i = 0; i < 4; i++) {
+        ptr++;
     }
-    sp20 = *(unkStruct2 *) sp2c;
-    sp34 = (sp20.unk01 & 0xc0) >> 4;
-    if (sp34 == 0) {
-        for (sp30 = 0; sp30 < 8; sp30++) {
-            *buffer++ = ((u8 *) &sp20.unk04)[sp30];
+
+    eepromformat = *(__OSContEepromFormat *) ptr;
+    ret = CHNL_ERR(eepromformat);
+
+    if (ret == 0) {
+        for (i = 0; i < 8; i++) {
+            *buffer++ = eepromformat.data[i];
         }
     }
+
     __osSiRelAccess();
-    return sp34;
+    return ret;
 }
 
-s32 __osPackEepReadData(u8 address) {
-    u8 *sp14;
-    unkStruct2 sp8;
-    s32 sp4;
-    sp14 = (u8 *) &__osEepPifRam;
-    for (sp4 = 0; sp4 < 0x10; sp4++) {
-        __osEepPifRam[sp4] = 255;
+void __osPackEepReadData(u8 address) {
+    u8 *ptr = (u8 *) &__osEepPifRam.ramarray;
+    __OSContEepromFormat eepromformat;
+    s32 i;
+    
+    for (i = 0; i < ARRAY_COUNT(__osEepPifRam.ramarray) + 1; i++) {
+        __osEepPifRam.ramarray[i] = CONT_CMD_NOP;
     }
-    D_8019769C = 1;
-    sp8.unk00 = 2;
-    sp8.unk01 = 8;
-    sp8.unk02 = 4;
-    sp8.unk03 = address;
-    for (sp4 = 0; sp4 < 8; sp4++) {
-        ((u8 *) &sp8.unk04)[sp4] = 0;
+    __osEepPifRam.pifstatus = CONT_CMD_EXE;
+
+    eepromformat.txsize = CONT_CMD_READ_EEPROM_TX;
+    eepromformat.rxsize = CONT_CMD_READ_EEPROM_RX;
+    eepromformat.cmd = CONT_CMD_READ_EEPROM;
+    eepromformat.address = address;
+    for (i = 0; i < ARRAY_COUNT(eepromformat.data); i++) {
+        eepromformat.data[i] = 0;
     }
-    for (sp4 = 0; sp4 < 4; sp4++) {
-        *sp14++ = 0;
+
+    for (i = 0; i < 4; i++) {
+        *ptr++ = 0;
     }
-    *(unkStruct2 *) sp14 = sp8;
-    sp14 += 0xc;
-    *sp14 = 254;
+
+    *(__OSContEepromFormat *) ptr = eepromformat;
+    ptr += sizeof(__OSContEepromFormat);
+    *ptr = CONT_CMD_END;
 }
