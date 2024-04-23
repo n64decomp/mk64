22a21c4fa629f968b35eb822267d6e47b81409f4
MegaMech <MegaMech@users.noreply.github.com>
2024-04-23 20:14:13 +0000

    Deploying to gh-pages from @ n64decomp/mk64@49625d397795441b8d81b9bf2f5e517094c273f2 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -32,8 +32,8 @@ var NAVTREE =
       [ "Memory Addresses", "md_docs_2doxygen__syms.html#memory-addresses-1", null ]
     ] ],
     [ "Tutorials", "tutorials.html", "tutorials" ],
-    [ "Bug List", "bug.html", null ],
     [ "Todo List", "todo.html", null ],
+    [ "Bug List", "bug.html", null ],
     [ "Data Structures", "annotated.html", [
       [ "Data Structures", "annotated.html", "annotated_dup" ],
       [ "Data Structure Index", "classes.html", null ],
