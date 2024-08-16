da2f95f53234c5f031c0597ad6840ab4355b23dd
MegaMech <MegaMech@users.noreply.github.com>
2024-08-16 21:49:49 +0000

    Deploying to gh-pages from @ n64decomp/mk64@52fd80f4cc620fb99dd3649ddd27cb9ce802210c 🚀

--- search/all_5.js
+++ search/all_5.js
@@ -604,7 +604,7 @@ var searchData=
   ['courseindex_601',['courseIndex',['../structstruct__8018EE10__entry.html#acee952c37d3917fd90809e58c6401e65',1,'struct_8018EE10_entry']]],
   ['courserecords_602',['courseRecords',['../structCupTimeTrialRecords.html#acf18ab0e69f90a2eefe248ef7090bd1c',1,'CupTimeTrialRecords']]],
   ['courses_603',['COURSES',['../mk64_8h.html#acf250ad4ca05ca435a61203c41d4ca88',1,'mk64.h']]],
-  ['courses_604',['Courses',['../courses.html',1,'Courses'],['../coursesmenu.html',1,'Courses'],['../courses.html#courses',1,'Courses'],['../controlflow.html#courses-1',1,'Courses']]],
+  ['courses_604',['Courses',['../courses.html',1,'Courses'],['../coursesmenu.html',1,'Courses'],['../courses.html#courses-1',1,'Courses'],['../controlflow.html#courses',1,'Courses']]],
   ['courses_2emd_605',['courses.md',['../courses_8md.html',1,'']]],
   ['coursesmenu_2emd_606',['coursesmenu.md',['../coursesmenu_8md.html',1,'']]],
   ['coursetable_607',['CourseTable',['../structCourseTable.html',1,'']]],
