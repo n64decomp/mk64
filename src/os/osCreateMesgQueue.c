bba709ee50417cef852b60fc58ee8fa0697539e2
coco875 <pereira.jannin@gmail.com>
2024-09-11 19:17:14 +0200

    Update osCreateMesgQueue.c

--- src/os/osCreateMesgQueue.c
+++ src/os/osCreateMesgQueue.c
@@ -1,8 +1,8 @@
 #include "libultra_internal.h"
 
 void osCreateMesgQueue(OSMesgQueue* mq, OSMesg* msgBuf, s32 count) {
-    mq->mtqueue = (OSThread*) &__osThreadTail.next;
-    mq->fullqueue = (OSThread*) &__osThreadTail.next;
+    mq->mtqueue = (OSThread *) &__osThreadTail.next;
+    mq->fullqueue = (OSThread *) &__osThreadTail.next;
     mq->validCount = 0;
     mq->first = 0;
     mq->msgCount = count;
