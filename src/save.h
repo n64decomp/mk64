0dbb7e34c0c91eaabe1a5292ba2b31200dfee80e
AloXado320 <david.albujar.s.30@gmail.com>
2024-10-05 13:29:04 -0500

    save.h

--- src/save.h
+++ src/save.h
@@ -14,9 +14,9 @@
 #define PFS_NO_ERROR 0        // controller pak no error return
 #define PFS_NO_PAK_INSERTED 1 // no pak pressent or wrong device inserted into the controller
 #define PFS_PAK_BAD_READ 2    // controller pak error while data transfer
-#define PFS_PAK_CORRUPTED 3   // controller pak has courrupted or weird data
+#define PFS_PAK_CORRUPTED 3   // controller pak has courrupted or has weird data
 #define PFS_FILE_OVERFLOW 4   // too many files written to in the controller pak
-#define PFS_INVALID_DATA -1   // bad arguments, game note does not exist, osPfsInit was not called.
+#define PFS_INVALID_DATA -1   // bad arguments, game note does not exist, osPfsInit was not called
 // controller pak test functions
 #define PFS_NUM_FILES_ERROR -2
 #define PFS_FREE_BLOCKS_ERROR -3
