41504a77aaa8e31cb0f0b01de1dd87937770f6d9
= <=>
2025-05-14 11:57:46 -0600

    Match regulate_cpu_speed for JP

--- src/cpu_vehicles_camera_path/cpu_speed_control.inc.c
+++ src/cpu_vehicles_camera_path/cpu_speed_control.inc.c
@@ -133,93 +133,114 @@ void regulate_cpu_speed(s32 playerId, f32 targetSpeed, Player* player) {
     s32 var_a1;
 
     speed = player->speed;
-    if (!(player->effects & 0x80) && !(player->effects & 0x40) && !(player->effects & 0x20000) &&
-        !(player->soundEffects & 0x400000) && !(player->soundEffects & 0x01000000) && !(player->soundEffects & 2) &&
-        !(player->soundEffects & 4)) {
-        if (gCurrentCourseId == COURSE_AWARD_CEREMONY) {
-            func_80007FA4(playerId, player, speed);
-        } else if ((bStopAICrossing[playerId] == true) && !(player->effects & (STAR_EFFECT | BOO_EFFECT))) {
-            decelerate_player(player, 10.0f);
-            if (player->currentSpeed == 0.0) {
-                player->velocity[0] = 0.0f;
-                player->velocity[2] = 0.0f;
-            }
-        } else {
-            var_f0 = 3.3333333f;
-            switch (gCCSelection) { /* irregular */
-                case CC_100:
-                case CC_EXTRA:
-                    break;
-                case CC_50:
-                    var_f0 = 2.5f;
-                    break;
-                case CC_150:
-                    var_f0 = 3.75f;
-                    break;
-            }
-            if (speed < var_f0) {
+
+    if (player->effects & 0x80 || player->effects & 0x40 || player->effects & 0x20000) {
+        return;
+    }
+
+    if (player->soundEffects & 0x400000 || player->soundEffects & 0x01000000) {
+        return;
+    }
+
+    if (player->soundEffects & 2 || player->soundEffects & 4) {
+        return;
+    }
+
+    if (gCurrentCourseId == COURSE_AWARD_CEREMONY) {
+        func_80007FA4(playerId, player, speed);
+        return;
+    }
+
+#ifdef VERSION_JP
+    check_ai_crossing_distance(playerId);
+#endif
+
+    if ((bStopAICrossing[playerId] == true) && !(player->effects & (STAR_EFFECT | BOO_EFFECT))) {
+#ifdef VERSION_JP
+        player->effects &= ~UNKNOWN_EFFECT_0x200000;
+#endif
+        decelerate_player(player, 10.0f);
+        if (player->currentSpeed == 0.0) {
+            player->velocity[0] = 0.0f;
+            player->velocity[2] = 0.0f;
+        }
+    } else {
+        var_f0 = 3.3333333f;
+        switch (gCCSelection) { /* irregular */
+            case CC_100:
+            case CC_EXTRA:
+                break;
+            case CC_50:
+                var_f0 = 2.5f;
+                break;
+            case CC_150:
+                var_f0 = 3.75f;
+                break;
+        }
+        if (speed < var_f0) {
+#ifndef VERSION_JP
+            player->effects &= ~UNKNOWN_EFFECT_0x200000;
+#endif
+            player_accelerate(player);
+        } else if (player->type & PLAYER_CINEMATIC_MODE) {
+            if (speed < targetSpeed) {
                 player->effects &= ~UNKNOWN_EFFECT_0x200000;
                 player_accelerate(player);
-            } else if (player->type & PLAYER_CINEMATIC_MODE) {
-                if (speed < targetSpeed) {
+            } else {
+                player->effects &= ~UNKNOWN_EFFECT_0x200000;
+                decelerate_player(player, 1.0f);
+            }
+        } else if ((D_801631E0[playerId] == true) && (D_80163330[playerId] != 1)) {
+            if (func_800088D8(playerId, gLapCountByPlayerId[playerId], gGPCurrentRaceRankByPlayerIdDup[playerId]) ==
+                1) {
+                player->effects |= UNKNOWN_EFFECT_0x200000;
+                player_accelerate(player);
+            } else {
+                player->effects &= ~UNKNOWN_EFFECT_0x200000;
+                decelerate_player(player, 1.0f);
+            }
+        } else {
+            var_a1 = 1;
+            switch (gSpeedCPUBehaviour[playerId]) { /* switch 1; irregular */
+                case SPEED_cpu_BEHAVIOUR_FAST:      /* switch 1 */
                     player->effects &= ~UNKNOWN_EFFECT_0x200000;
                     player_accelerate(player);
-                } else {
-                    player->effects &= ~UNKNOWN_EFFECT_0x200000;
-                    decelerate_player(player, 1.0f);
-                }
-            } else if ((D_801631E0[playerId] == true) && (D_80163330[playerId] != 1)) {
-                if (func_800088D8(playerId, gLapCountByPlayerId[playerId], gGPCurrentRaceRankByPlayerIdDup[playerId]) ==
-                    1) {
+                    break;
+                case SPEED_cpu_BEHAVIOUR_MAX: /* switch 1 */
                     player->effects |= UNKNOWN_EFFECT_0x200000;
                     player_accelerate(player);
-                } else {
-                    player->effects &= ~UNKNOWN_EFFECT_0x200000;
-                    decelerate_player(player, 1.0f);
-                }
-            } else {
-                var_a1 = 1;
-                switch (gSpeedCPUBehaviour[playerId]) { /* switch 1; irregular */
-                    case SPEED_cpu_BEHAVIOUR_FAST:      /* switch 1 */
-                        player->effects &= ~UNKNOWN_EFFECT_0x200000;
+                    break;
+                case SPEED_cpu_BEHAVIOUR_SLOW: /* switch 1 */
+                    if (((speed / 18.0f) * 216.0f) > 20.0f) {
+                        targetSpeed = 1.6666666f;
+                    }
+                    var_a1 = 0;
+                    break;
+                case SPEED_cpu_BEHAVIOUR_NORMAL: /* switch 1 */
+                default:                         /* switch 1 */
+                    var_a1 = 0;
+                    break;
+            }
+            if (var_a1 != 1) {
+                if (speed < targetSpeed) {
+                    if ((gDemoMode == 1) && (gCurrentCourseId != COURSE_AWARD_CEREMONY)) {
                         player_accelerate(player);
-                        break;
-                    case SPEED_cpu_BEHAVIOUR_MAX: /* switch 1 */
+                    } else if (D_80163330[playerId] == 1) {
+                        func_80007D04(playerId, player);
+                    } else if (func_800088D8(playerId, gLapCountByPlayerId[playerId],
+                                                gGPCurrentRaceRankByPlayerIdDup[playerId]) == true) {
                         player->effects |= UNKNOWN_EFFECT_0x200000;
                         player_accelerate(player);
-                        break;
-                    case SPEED_cpu_BEHAVIOUR_SLOW: /* switch 1 */
-                        if (((speed / 18.0f) * 216.0f) > 20.0f) {
-                            targetSpeed = 1.6666666f;
-                        }
-                        var_a1 = 0;
-                        break;
-                    case SPEED_cpu_BEHAVIOUR_NORMAL: /* switch 1 */
-                    default:                         /* switch 1 */
-                        var_a1 = 0;
-                        break;
-                }
-                if (var_a1 != 1) {
-                    if (speed < targetSpeed) {
-                        if ((gDemoMode == 1) && (gCurrentCourseId != COURSE_AWARD_CEREMONY)) {
-                            player_accelerate(player);
-                        } else if (D_80163330[playerId] == 1) {
-                            func_80007D04(playerId, player);
-                        } else if (func_800088D8(playerId, gLapCountByPlayerId[playerId],
-                                                 gGPCurrentRaceRankByPlayerIdDup[playerId]) == true) {
-                            player->effects |= UNKNOWN_EFFECT_0x200000;
-                            player_accelerate(player);
-                        } else {
-                            player->effects &= ~UNKNOWN_EFFECT_0x200000;
-                            decelerate_player(player, 1.0f);
-                        }
                     } else {
                         player->effects &= ~UNKNOWN_EFFECT_0x200000;
-                        if (targetSpeed > 1.0f) {
-                            decelerate_player(player, 2.0f);
-                        } else {
-                            decelerate_player(player, 5.0f);
-                        }
+                        decelerate_player(player, 1.0f);
+                    }
+                } else {
+                    player->effects &= ~UNKNOWN_EFFECT_0x200000;
+                    if (targetSpeed > 1.0f) {
+                        decelerate_player(player, 2.0f);
+                    } else {
+                        decelerate_player(player, 5.0f);
                     }
                 }
             }
